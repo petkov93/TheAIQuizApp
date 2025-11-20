82aedcc1f420906390343a80022c9047c13974c7
petkov93 <petko.petkov19931123@gmail.com>
2025-11-21 00:14:45 +0100

    added custom submit btn

--- ui/base_widgets/submit_button.py
+++ ui/base_widgets/submit_button.py
@@ -1,11 +1,15 @@
 from customtkinter import CTkButton
 
+from ui.styles.fonts import submit_button_font
+
 
 class SubmitButton(CTkButton):
+
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs,
                          hover_color='dark green',
                          fg_color='forest green',
-                         font=('Aerial', 14), width=200,
+                         font=submit_button_font,
+                         width=200,
                          height=50)
 
