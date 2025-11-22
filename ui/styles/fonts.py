5f4bbd36cde02cf4e0826f6b44c4f2492a54e109
petkov93 <petko.petkov19931123@gmail.com>
2025-11-22 07:30:46 +0100

    added more fonts

--- ui/styles/fonts.py
+++ ui/styles/fonts.py
@@ -1,5 +1,8 @@
 title_font = ('Aerial', 30, 'bold')
 middle_font = ('Aerial', 20)
 small_font = ('Aerial', 16, 'bold')
+nav_font = font=('Aerial', 14, 'bold')
 
 submit_button_font = ('Aerial', 20, 'bold')
+
+radio_button_font = ('Courier New', 14)
