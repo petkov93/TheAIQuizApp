e27fd21a398b57ae883f7598a8f2fb240800634f
petkov93 <petko.petkov19931123@gmail.com>
2025-11-22 07:36:56 +0100

    refactor

--- ui/frames/start_quiz_frame.py
+++ ui/frames/start_quiz_frame.py
@@ -7,17 +7,16 @@ from ui.base_widgets.submit_button import SubmitButton
 class StartQuizFrame(BaseFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.load_widgets()
 
     def load_widgets(self):
         BigLabel(self, text=f'Generated Quiz on:')
         MidLabel(self, textvariable=self.topic_var)
-        MidLabel(self, text=f'Total questions: {self.total_questions}')
+        MidLabel(self, text=f'Total questions: {self.total_questions_var.get()}')
         SubmitButton(self, text='Start Quiz!', command=self.start_quiz)
 
         for child in self.winfo_children():
             child.pack(anchor='center', pady=20)
 
     def start_quiz(self):
-        self.on_next_frame()
+        self.controller.next_frame()
