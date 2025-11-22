f27dffc3b3128e18313ff599f2be9f302e9f601d
petkov93 <petko.petkov19931123@gmail.com>
2025-11-22 07:24:39 +0100

    refactor, type annotations

--- ui/base/base_frame.py
+++ ui/base/base_frame.py
@@ -1,22 +1,16 @@
 from abc import ABC, abstractmethod
-from collections.abc import Callable
 
 from customtkinter import CTkFrame
 
-from core.controller import QuizController
-from core.quiz_manager import QuizManager
-
 
 class BaseFrame(CTkFrame, ABC):
-    def __init__(self, master, controller: QuizController, quiz_manager: QuizManager, on_next_frame: Callable, *args,
-                 **kwargs):
+    def __init__(self, master, controller, *args, **kwargs):
         super().__init__(master, fg_color='transparent', *args)
         self.controller = controller
-        self.quiz_manager = quiz_manager
-        self.on_next_frame = on_next_frame
-        self.topic_var = kwargs['topic_var']
-        self.score_var = kwargs['score_var']
-        self.total_questions = kwargs['total_questions']
+        self.topic_var = kwargs.get('topic_var', '')
+        self.score_var = kwargs.get('score_var', 0)
+        self.question_number_var = kwargs.get('question_number_var', 0)
+        self.total_questions_var = kwargs.get('total_questions_var', 0)
 
     @abstractmethod
     def load_widgets(self):
