5bf4ca1e4e4914dc17f88e8f2152b4928afb499e
petkov93 <petko.petkov19931123@gmail.com>
2025-11-22 07:12:50 +0100

    added type annotations

--- core/question.py
+++ core/question.py
@@ -1,11 +1,10 @@
 class Question:
     def __init__(self, question_data: dict):
-        self.question = question_data['question']
-        self.options = question_data['options']
-        self.correct_answer = question_data['answer']
-        self.user_answer = -1
-        self.explanation = question_data['explanation']
-        self.is_answered = False
+        self.question: str = question_data['question']
+        self.options: list[str] = question_data['options']
+        self.correct_answer: int = question_data['answer']
+        self.explanation: str = question_data['explanation']
+        self.user_answer: int = -1
 
     def check_answer(self, selected_index) -> bool:
         """Checks if the selected answer is the same as the correct answer. Returns True or False"""
