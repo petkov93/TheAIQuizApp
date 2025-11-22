df9ec483054fe05bb18048a584fc58c499056160
petkov93 <petko.petkov19931123@gmail.com>
2025-11-22 06:57:13 +0100

    added methods so the controller can interact with the manager

--- core/controller.py
+++ core/controller.py
@@ -14,4 +14,25 @@ class QuizController:
 
     def restart_quiz(self):
         self.quiz_manager.restart()
-        self.frame_index = 0
+
+    def get_current_question(self):
+        return self.quiz_manager.current_question
+
+    def get_total_questions(self):
+        return self.quiz_manager.total_questions
+
+    def change_question(self, index: int):
+        self.quiz_manager.go_to_question(index)
+
+    def submit_answer(self, selected_index: int):
+        return self.quiz_manager.submit_answer(selected_index)
+
+    def get_final_score(self):
+        return self.quiz_manager.get_final_score()
+
+    def update_shared_vars(self):
+        manager = self.quiz_manager
+        self.window.topic_var.set(self.window.topic_var.get())
+        self.window.total_questions_var.set(manager.total_questions)
+        self.window.question_number_var.set(manager.current_question_number)
+        self.window.score_var.set(manager.score)
