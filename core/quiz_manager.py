e6ea14b7f296187e15de7ef030083fbcea37b80a
petkov93 <petko.petkov19931123@gmail.com>
2025-11-22 07:15:30 +0100

    added @properties to the manager class

--- core/quiz_manager.py
+++ core/quiz_manager.py
@@ -5,14 +5,55 @@ from core.question_fetcher import QuestionFetcher
 
 
 class QuizManager:
-    def __init__(self, total_questions: int):
-        self.topic = 'No topic selected.'
-        self.score = 0
-        self.all_questions: list[Question] = []
-        # TODO use this list to show the user which Questions he got wrong
-        self.wrong_answers = []
-        self.total_questions = total_questions
-        self.answered = set()
+    def __init__(self):
+        self._questions: list[Question] = []
+        self._current_index: int = 0
+        self._score: int = 0
+        self._answered_indices: set[int] = set()
+
+    @property
+    def score(self) -> int:
+        return self._score
+
+    @property
+    def total_questions(self):
+        return len(self._questions)
+
+    @property
+    def current_question(self) -> Question:
+        if not self._questions:
+            raise ValueError("No questions loaded yet!")
+        return self._questions[self._current_index]
+
+    @property
+    def current_question_number(self) -> int:
+        return self._current_index + 1
+
+    @property
+    def is_finished(self) -> bool:
+        return len(self._questions) == len(self._answered_indices)
+
+    # quiz logic
+    def submit_answer(self, selected_index: int) -> bool:
+        """Submit answer for current question. Returns True if correct."""
+        if self._current_index in self._answered_indices:
+            raise ValueError("Already Answered!")
+
+        is_correct = self.current_question.check_answer(selected_index)
+        self._answered_indices.add(self._current_index)
+        if is_correct:
+            self._score += 1
+
+        return is_correct
+
+    def next_question(self) -> None:
+        if not self.is_finished:
+            while self._current_index in self._answered_indices:
+                self._current_index = (self._current_index + 1) % len(self._questions)
+
+    def go_to_question(self, index: int) -> None:
+        if 0 <= index < len(self._questions):
+            self._current_index = index
 
     # TODO remove the class from here, put it in __init__, create the obj in main.py
     def fetch_questions(self, on_complete):
