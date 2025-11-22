da7d8a800072043b17dea4e00000a8c66c9bd25d
petkov93 <petko.petkov19931123@gmail.com>
2025-11-22 07:26:56 +0100

    implemented RadioButton

--- ui/base_widgets/radio_button.py
+++ ui/base_widgets/radio_button.py
@@ -1,5 +1,11 @@
 from customtkinter import CTkRadioButton
 
+from ui.styles.fonts import radio_button_font
+
 
 class RadioButton(CTkRadioButton):
-    pass
+    FONT = radio_button_font
+
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs,
+                         font=self.FONT)
